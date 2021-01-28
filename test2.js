90ff0268f2544be9f7a5002f63636ff2753bda03
khuebc <khuebcc@gmail.com>
2021-01-28 23:27:22 +0700

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,5 @@
+console.log('Hello World from Webguard test 1');
+console.log('Hello World from Webguard test 2');
+console.log('Hello World from Webguard test 3');
+console.log('Hello World from Webguard test 4');
+console.log('Hello World from Webguard test 5');
