de16db0c830e61b7a23c875d236e514b3f5b1beb
khuebc <khuebcc@gmail.com>
2021-02-26 19:00:01 +0700

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-console.log('Hello World from Webguard test 1');
-console.log('Hello World from Webguard test 2');
-console.log('Hello World from Webguard test 3');
-console.log('Hello World from Webguard test 4');
-console.log('Hello World from Webguard test 5');
