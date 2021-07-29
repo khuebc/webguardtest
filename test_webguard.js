0bbb2fbb7a9c43c5e7861c9c99139520a81169c4
khuebc <khuebcc@gmail.com>
2021-07-29 06:08:38 -0500

    Create test_webguard.js

---
+++ test_webguard.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#top_menu_collapse').find('.nav-link').first().html('<span>First</span>');
+    $('#top_menu_collapse').find('.nav-link').eq(1).html('<span>Second</span>');
+    $('#top_menu_collapse').find('.nav-link').eq(2).html('<span>Third</span>');
+});
