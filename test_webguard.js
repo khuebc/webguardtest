4c63004e4b85bd1aa6104d5c5fe643f6d515b213
khuebc <khuebcc@gmail.com>
2021-07-29 06:26:16 -0500

    Update test_webguard.js

--- test_webguard.js
+++ test_webguard.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
-    $('#top_menu_collapse').find('.nav-link').first().html('<span>First</span>');
-    $('#top_menu_collapse').find('.nav-link').eq(1).html('<span>Second</span>');
-    $('#top_menu_collapse').find('.nav-link').eq(2).html('<span>Third</span>');
+document.addEventListener("DOMContentLoaded", function(event) { 
+   document.getElementById('top_menu').children[0].querySelector('span').innerHTML = '<span>First</span>';
+    document.getElementById('top_menu').children[1].querySelector('span').innerHTML = '<span>Second</span>';
+    document.getElementById('top_menu').children[2].querySelector('span').innerHTML = '<span>Third</span>';
+    document.getElementById('top_menu').children[3].querySelector('span').innerHTML = '<span>Fourth</span>';
 });
