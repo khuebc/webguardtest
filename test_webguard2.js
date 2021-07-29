c1e83536112ec2e24d5c710861f21426c51938b4
khuebc <khuebcc@gmail.com>
2021-07-29 06:28:48 -0500

    Create test_webguard2.js

---
+++ test_webguard2.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+   document.getElementById('top_menu').children[0].querySelector('span').innerHTML = '<span>First</span>';
+    document.getElementById('top_menu').children[1].querySelector('span').innerHTML = '<span>Second</span>';
+    document.getElementById('top_menu').children[2].querySelector('span').innerHTML = '<span>Third</span>';
+    document.getElementById('top_menu').children[3].querySelector('span').innerHTML = '<span>Fourth</span>';
+});
