15735128fb8308ebba375642b7e5d5f53f9496ce
khuebc <khuebcc@gmail.com>
2021-08-20 19:22:13 +0700

    Update test_webguard4.js

--- test_webguard4.js
+++ test_webguard4.js
@@ -11,6 +11,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById('second-img').src = '/mywebguard/static/src/img/mywebguard.png';
     document.getElementById('third-img').src = '/mywebguard/static/src/img/mywebguard.png';
     
+    
+    // XHR
     var data = JSON.stringify({});
 
     var xhr = new XMLHttpRequest();
@@ -18,14 +20,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     xhr.addEventListener("readystatechange", function () {
       if (this.readyState === 4) {
-        console.log(this.responseText);
+        document.getElementById('first-text').innerHTML = (JSON.parse(this.responseText).result.data.text);
+        document.getElementById('second-text').innerHTML = (JSON.parse(this.responseText).result.data.text);
+        document.getElementById('third-text').innerHTML = (JSON.parse(this.responseText).result.data.text);
       }
     });
 
     xhr.open("POST", "http://localhost:8076/mywebguard/tes_api");
     xhr.setRequestHeader("content-type", "application/json");
     xhr.setRequestHeader("cache-control", "no-cache");
-//     xhr.setRequestHeader("postman-token", "9eb47305-288b-6c37-139e-42b0db7d132e");
 
     xhr.send(data);
 
